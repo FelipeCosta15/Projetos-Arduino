498d13e5d6f7ca8aee9f65b696d11617aab93dbc
Felipe Sartori Costa <sartorifelipecosta@gmail.com>
2024-08-05 16:29:30 -0300

    liga e desliga

---
+++ piscar_2_leds/piscar_2_leds.ino
@@ -0,0 +1,19 @@
+// Progama que envia um sinal alto para a saída 7
+// aguarda para enviar um sinal baixo pra a saída 7
+void setup() {
+    // put your setup code here, to run once:
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7,HIGH);
+  delay(1000);
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(7,LOW);
+  delay(1000);
+  digitalWrite(8,LOW);
+  delay(1000);
+}
